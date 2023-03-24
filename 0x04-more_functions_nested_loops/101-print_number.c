fea157953dfbbf503549733df550d617757fc5a7
probrahim <ibrahimhassani674@gmail.com>
2023-03-24 11:07:26 -0700

    3layn twaden

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,20 @@
 #include "main.h"
-
 /**
- * print_number - prints an integer
- * @n: input integer parameter
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-void print_number(int n)
+int main(void)
 {
-		unsigned int i = n;
-
-			if (n < 0)
-					{
-								_putchar(45);
-										i = -i;
-											}
-				if (i / 10)
-						{
-									print_number(i / 10);
-										}
-					_putchar(i % 10 + '0');
+print_number(98);
+_putchar('\n');
+print_number(402);
+_putchar('\n');
+print_number(1024);
+_putchar('\n');
+print_number(0);
+_putchar('\n');
+print_number(-98);
+_putchar('\n');
+return (0);
 }

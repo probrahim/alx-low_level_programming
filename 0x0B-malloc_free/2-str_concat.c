d24c8023936e7fa4d96b9a30a7edabdeeac5d807
probrahim <ibrahimhassani674@gmail.com>
2023-11-21 21:34:00 +0100

    q

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,41 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat		
- * Return: concat of s1 and s2		
- */ 
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
+ *
+ * Return: If concatenation fails - NULL.
+ *         Otherwise - a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
+ */
 char *str_concat(char *s1, char *s2)
 {
-	char *psd;
-	int i;
-	int t;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	i = t = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[t] != '\0')
-		t++;
-	psd = malloc(sizeof(char) * (i + t + 1));
 
-	if (psd == NULL)
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
 		return (NULL);
-	i = t = 0;
-		while (s1[i] != '\0')
-	{
-		psd[i] = s1[i];
-		i++;
-	}
-	while (s2[t] != '\0')
-	{
-		psd[i] = s2[t];
-		i++, t++;
-	}
-	psd[i] = '\0';
-	return (psd);
+
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
 }

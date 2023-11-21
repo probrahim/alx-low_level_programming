d34d637b5773a3d7caab056a197cc2a77fc4c3cf
probrahim <ibrahimhassani674@gmail.com>
2023-11-21 21:53:34 +0100

    1

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,11 +11,11 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-    i = 0;
-    while (i < height)
-    {
-        free(grid[i]);
-        i++;
-    }
-	free(grid);
+i = 0;
+while (i < height)
+{
+free(grid[i]);
+i++;
+}
+free(grid);
 }

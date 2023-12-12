9ddee87cde20cb4d9f9d7a0f2ab6847469be007e
probrahim <ibrahimhassani674@gmail.com>
2023-12-12 18:57:40 +0100

    1

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: double pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *base;
+
+	while (*head != NULL)
+	{
+		base = *head;
+		*head = (*head)->next;
+		free(base);
+	}
+}

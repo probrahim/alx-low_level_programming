d3b1a019e71f973cca2a61175e4c91137373d924
probrahim <ibrahimhassani674@gmail.com>
2024-01-09 12:43:03 +0100

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,10 @@ struct dog
  */
 typedef struct dog dog_t;
 
+/* lib--*/
+#include <stdlib.h>
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

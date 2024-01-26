ab19bac7447597302d4d4221a91ddb72138f9a13
probrahim <ibrahimhassani674@gmail.com>
2024-01-26 21:57:56 +0100

    Update changes

---
+++ 1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Counts the number 
+ * @h: The head 
+ * Return: The number
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t pose = 0;
+
+	while (h)
+	{
+		pose++;
+		h = h->next;
+	}
+
+	return (pose);
+}

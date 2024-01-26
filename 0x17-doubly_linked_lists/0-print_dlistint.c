6cd228ef66233005baeb1c7dbbe6869fe8f42818
probrahim <ibrahimhassani674@gmail.com>
2024-01-26 21:44:15 +0100

    0x17-doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all the elements.
+ * @h: The head of the dlistint_t list.
+ * Return: The number.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t pose = 0;
+
+	while (h)
+	{
+		pose++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (pose);
+}

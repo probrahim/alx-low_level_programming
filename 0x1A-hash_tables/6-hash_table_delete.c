90ffe2dbe384ae06dc280ff4e0cd98da4577de4a
probrahim <ibrahimhassani674@gmail.com>
2024-02-19 17:32:20 +0100

    task 06

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int p;
+
+	for (p = 0; p < ht->size; p++)
+	{
+		if (ht->array[p] != NULL)
+		{
+			node = ht->array[p];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}

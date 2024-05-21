3f928245fef9e358be35486924906cf39de96d1c
probrahim <ibrahimhassani674@gmail.com>
2024-05-21 18:41:00 +0200

    fix task 06

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+
+
+
+int jump_search(int *array, size_t size, int value)
+{
+	int i, a, b, ins;
+
+	if (array == NULL)
+		return (-1);
+	if (size == 0)
+		return (-1);
+
+	a = (int)sqrt((double)size);
+	b = 0;
+	ins = i = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
+		b++;
+		ins = i;
+		i = b * a;
+	} while (i < (int)size && array[i] < value);
+	printf("Value found between indexes [%d] and [%d]\n", ins, i);
+
+	for (; ins <= i && ins < (int)size; ins++)
+	{
+		printf("Value checked array[%d] = [%d]\n", ins, array[ins]);
+		if (array[ins] == value)
+			return (ins);
+	}
+	return (-1);
+}
